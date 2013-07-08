0c9c0bb8f8c93626a7c7d6ed2234d6fe22b03c42
b0nd <b0nd.g4h@gmail.com>
2013-07-08 11:45:01 +0530

    Refactored code. Removed scaffolds and outdated comments.

--- ShivaReceiver/iReceiver/config/settings.py
+++ ShivaReceiver/iReceiver/config/settings.py
@@ -1,27 +1,12 @@
 # This file contains python variables that configure Lamson for email processing.
-import logging
-
-
-#debugFd = open("/root/Desktop/LamsonHoneyMail/MyMailServer/debug.txt", 'a')
-#debugFd.write("------------------------------------\n")
-
 # You may add additional parameters such as `username' and `password' if your
 # relay server requires authentication, `starttls' (boolean) or `ssl' (boolean)
 # for secure connections.
+
 relay_config = {'host': 'localhost', 'port': 8825}
-#relay_config = {'host': '10.91.1.144', 'port': 25}
 
-
-#receiver_config = {'maildir': './run/undeliverable/'}
-#handlers = ['app.handlers.sample']
-
-#receiver_config = {'host': 'localhost', 'port': 8823}
-#receiver_config = {'host': '14.140.125.209', 'port': 25}
 receiver_config = {'host': '192.168.6.97', 'port': 25}
 
-
-#handlers = ['app.handlers.sample']
-#handlers = ['app.handlers.spampot', 'app.handlers.undeliveredFilter']
 handlers = ['app.handlers.spampot']
 
 
@@ -29,9 +14,5 @@ router_defaults = {'host': '.+'}
 template_config = {'dir': 'app', 'module': 'templates'}
 
 # the config/boot.py will turn these values into variables set in settings
-
-#debugFd.write("[+] Finished parsing setting.py file\n")
-#debugFd.close()
-
 
 
